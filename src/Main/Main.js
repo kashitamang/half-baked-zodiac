c6b62e23ce86b2c3724e280f28073bc769211fb5
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-06 16:51:06 -0700

    adds ... method to loop through data

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -11,10 +11,9 @@ export default function Main() {
       {/* get the zodiac array from props */}
       {
         zodiac.map((zodiac, i) => <ZodiacCard 
-          name={zodiac.name}
-          dates={zodiac.dates}
+          {...zodiac}
           key={zodiac.name + i}/>)
-      }
+      };
       {/* for each item in the `zodiacs` array props, render a ZodiacCard component, 
       passing in the name and the dates as props to the ZodiacCard component */}
       {/* remember that when you map over an array and render out a list, 
