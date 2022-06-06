2dbcaa7f2155ac7c5bfa0454bd2b9e90d3a40c52
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-06 15:18:03 -0700

    adds styling t page

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -4,8 +4,8 @@ export default function ZodiacCard({ name, dates }) {
   /* in this component,  use the `name` and `dates` props to render the name and dates of the sign, 
   as well as load the correct image from the public directory */
   return (
-    <div className="zodiac">
-      <p>{name}</p>
+    <div className="zodiac-card">
+      <p className="name">{name}</p>
       <p>{dates}</p>
       <img src={`/images/${name}.png`}/>
     </div>
